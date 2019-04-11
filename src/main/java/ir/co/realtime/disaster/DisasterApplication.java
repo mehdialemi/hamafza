42b78bacd197b64c4b3807360fe6347eac70e448
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2019-04-11 12:11:26 +0430

    init

---
+++ src/main/java/ir/co/realtime/disaster/DisasterApplication.java
@@ -0,0 +1,13 @@
+package ir.co.realtime.disaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterApplication.class, args);
+	}
+
+}
